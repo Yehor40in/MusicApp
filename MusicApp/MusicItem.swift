c5f35b9dcbaea989d89795236410bcff4b90f08b
Yehor Sorokin <ysorokin@macmini184.softservecom.com>
2019-10-02 14:40:20 +0300

    Add MusicListController, Create Playing View, IMplement Model for MusicItem

---
+++ MusicApp/MusicItem.swift
@@ -0,0 +1,31 @@
+//
+//  MusicItem.swift
+//  MusicApp
+//
+//  Created by Yehor Sorokin on 10/1/19.
+//  Copyright © 2019 Yehor Sorokin. All rights reserved.
+//
+
+import Foundation
+
+
+class MusicItem {
+    
+    //MARK: - Properties
+    var image: String?
+    var artist: String
+    var name: String
+    var location: URL!
+    var dateAdded: Date!
+    
+    
+    init(image: String? = nil, artist: String, name: String) {
+        if let temp = image {
+            self.image = temp
+        }
+        
+        self.image = image
+        self.artist = artist
+        self.name = name
+    }
+}
--- MusicApp/ViewController.swift
+++ MusicApp/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var model = MusicListModel()
+    
+    //MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var nowPlaying: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.tableView!.dataSource = self.model
     }
+}
 
 
+//MARK: TableView Delegate
+extension ViewController: UITableViewDelegate {
+    
+    //
+    
 }
 
