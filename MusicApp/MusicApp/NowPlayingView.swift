56d5e83208eeb654e11ef7c37c760d318e2aadc0
Yehor <y.sorokin1996@gmail.com>
2019-10-02 16:01:01 +0300

    Add files via upload

---
+++ MusicApp/MusicApp/NowPlayingView.swift
@@ -0,0 +1,14 @@
+//
+//  NowPlayingView.swift
+//  MusicApp
+//
+//  Created by Yehor Sorokin on 10/1/19.
+//  Copyright © 2019 Yehor Sorokin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NowPlayingView: UIView {
+    
+}
