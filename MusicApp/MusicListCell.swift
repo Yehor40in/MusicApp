4cb81a176b1fb78e4538347a3295dc06c2ea9ffe
Yehor <y.sorokin1996@gmail.com>
2019-10-16 16:01:18 +0300

    Update MusicListCell.swift

--- MusicApp/MusicListCell.swift
+++ MusicApp/MusicListCell.swift
@@ -33,10 +33,4 @@ class MusicListCell: UITableViewCell {
         self.songCover.layer.cornerRadius = 8
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
