2b233b2590a93f5b9787a4f75257b891f1d70bc9
Yehor <y.sorokin1996@gmail.com>
2019-10-17 11:33:00 +0300

    Update MusicListController.swift

--- MusicApp/MusicListController.swift
+++ MusicApp/MusicListController.swift
@@ -1,110 +1,104 @@
 //
-//  MusicListControllerUtilities.swift
+//  MusicListController.swift
 //  MusicApp
 //
-//  Created by Yehor Sorokin on 10/4/19.
+//  Created by Yehor Sorokin on 10/2/19.
 //  Copyright © 2019 Yehor Sorokin. All rights reserved.
 //
 
-import AVFoundation
 import UIKit
+import AVFoundation
 
-extension MusicListController {
-    //MARK: - Utilities
+
+class MusicListController: UIViewController {
     
-    //sample data for now
-    func getItems() -> [MusicItem] {
-        return [
-            MusicItem(artist: "Bensound", name: "Dubstep", path: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Samples/bensound-dubstep", ofType: "mp3")!)),
-            MusicItem(artist: "Aensound", name: "Energy", path: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Samples/bensound-energy", ofType: "mp3")!)),
-            MusicItem(artist: "Censound", name: "Epic", path: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Samples/bensound-epic", ofType: "mp3")!)),
-            MusicItem(artist: "Fensound", name: "Once Again", path: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Samples/bensound-onceagain", ofType: "mp3")!))
-        ]
+    //MARK: - Properties
+    var items: [MusicItem]?
+    var sectionTitles: [String]?
+    
+    var player: AVAudioPlayer!
+    var paused = false
+    
+    var playingRow: Int?
+    
+    
+    //MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var playingView: UIView!
+    @IBOutlet weak var playingCover: UIImageView!
+    @IBOutlet weak var playingName: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.items = preparedItems(from: getItems(), by: .title)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        playingName.text = "Not Playing"
+        playingCover.image = UIImage(named: "defaultmusicicon")
+        playingCover.layer.cornerRadius = 8
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
     }
+
     
-    
-    func preparedItems(from raw: [MusicItem], by option: SortOption) -> [MusicItem] {
-        var prepared = [MusicItem]()
-        
-        self.sectionTitles = [String]()
-        
-        switch option {
-        case .artist:
-            prepared = raw.sorted { $0.artist < $1.artist }
-            _ = prepared.map {
-                sectionTitles!.append(String($0.artist.first!))
-            }
-        case .title:
-            prepared = raw.sorted { $0.name < $1.name }
-            _ = prepared.map {
-                sectionTitles!.append(String($0.name.first!))
-            }
-        case .date:
-            prepared = raw.sorted { $0.dateAdded < $1.dateAdded }
-            for (index, _) in prepared.enumerated() {
-                sectionTitles!.append("\(index)")
-            }
+    //MARK: - Actions
+    @IBAction func playButtonTapped(_ sender: Any) {
+        if player == nil {
+            self.playRandomSong()
+            
+        } else if paused {
+            self.paused = false
+            self.player.play()
+            self.playButton.setImage(UIImage(systemName: "pause.fill", withConfiguration: .none), for: .normal)
+            
+        } else {
+            self.paused = true
+            self.player.pause()
+            self.playButton.setImage(UIImage(systemName: "play.fill", withConfiguration: .none), for: .normal)
         }
-        return prepared
     }
     
     
-    func prepareMusicAndSession(for index: Int) {
-        let item = items?[index]
+    @IBAction func forwardTapped(_ sender: Any) {
+        playRandomSong()
+    }
+    
+    
+    @IBAction func sortTapped(_ sender: Any) {
+        let actionSheet = UIAlertController(title: nil, message: "Sort by", preferredStyle: .actionSheet)
         
-        player = try! AVAudioPlayer(contentsOf: item!.location)
-        player.delegate = self
-        player.prepareToPlay()
-        
-        let session = AVAudioSession.sharedInstance()
-        do {
-            try session.setCategory(AVAudioSession.Category.playback)
-        } catch let sessionError {
-            print(sessionError)
-        }
-        self.forwardButton.isEnabled = true
-    }
-    
-    
-    func updatePlayingView(with object: MusicItem) {
-        self.playingCover.image = UIImage(named: object.image!)
-        self.playingName.text = object.name
-        self.playButton.setImage(UIImage(systemName: "pause.fill", withConfiguration: .none), for: .normal)
-    }
-    
-    
-    func playRandomSong() {
-        let row = Int.random(in: 0..<(items?.count)!)
-        
-        prepareMusicAndSession(for: row)
-        self.player.play()
-        updatePlayingView(with: items![row])
-    }
-}
+        actionSheet.addAction(UIAlertAction(title: "Artist", style: .default, handler: { (_) in
+            self.items = self.preparedItems(from: self.items!, by: .artist)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }))
 
+        actionSheet.addAction(UIAlertAction(title: "Title", style: .default, handler: { (_) in
+            self.items = self.preparedItems(from: self.items!, by: .title)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }))
 
-extension MusicListController: UITableViewDataSource {
-    // MARK: - Table view data source
+        actionSheet.addAction(UIAlertAction(title: "Recently Added", style: .default, handler: { (_) in
+            self.items = self.preparedItems(from: self.items!, by: .date)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }))
 
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
+        actionSheet.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: { (_) in
+            self.dismiss(animated: true, completion: nil)
+        }))
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items!.count
+        self.present(actionSheet, animated: true, completion: nil)
     }
 
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MusicListCell", for: indexPath) as! MusicListCell
-        cell.item = items?[indexPath.row]
-        
-        return cell
-    }
-    
-    
-    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-        return sectionTitles
-    }
 }
