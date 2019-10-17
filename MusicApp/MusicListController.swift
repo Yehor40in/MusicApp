c454bb209c6c902ca930f78e01c4607d7fd71e9b
Yehor <y.sorokin1996@gmail.com>
2019-10-17 10:26:43 +0300

    Update MusicListController.swift

--- MusicApp/MusicListController.swift
+++ MusicApp/MusicListController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Yehor Sorokin. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class MusicListController: UIViewController {
@@ -69,3 +70,45 @@ class MusicListController: UIViewController {
     }
 
 }
+
+
+extension MusicListController: UITableViewDataSource {
+    // MARK: - Table view data source
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionTitles!.count
+    }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let key = Character(sectionTitles![section])
+        if let item = items?[key] {
+            return item.count
+        } else {
+            return 0
+        }
+    }
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MusicListCell", for: indexPath) as! MusicListCell
+        let key = Character(sectionTitles![indexPath.section])
+        
+        if let item = items?[key] {
+            cell.item = item[indexPath.row]
+        }
+        return cell
+    }
+    
+    
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return sectionTitles
+    }
+    
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles?[section]
+    }
+}
+
