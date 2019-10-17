6e494dc38513fd64278752f15cfb126041e1341b
Yehor Sorokin <ysorokin@macmini184.softservecom.com>
2019-10-17 12:07:48 +0300

    Refactor code a bit

--- MusicApp/MusicListController/MusicListController.swift
+++ MusicApp/MusicListController/MusicListController.swift
@@ -113,3 +113,38 @@ class MusicListController: UIViewController {
     }
 
 }
+
+
+extension MusicListController: UITableViewDataSource {
+    // MARK: - Table view data source
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.sectionTitles!.count
+    }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let key = Character(sectionTitles![section])
+        return self.items![key]!.count
+    }
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let key = Character(sectionTitles![indexPath.section])
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MusicListCell", for: indexPath) as! MusicListCell
+        cell.item = self.items![key]![indexPath.row]
+        
+        return cell
+    }
+    
+    
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return self.sectionTitles
+    }
+    
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.sectionTitles![section]
+    }
+}
