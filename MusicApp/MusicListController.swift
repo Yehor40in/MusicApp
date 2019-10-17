4e400ac639d840b0c2b030ba007c196f0d5572ef
Yehor <y.sorokin1996@gmail.com>
2019-10-17 23:57:43 +0300

    Update MusicListController.swift

--- MusicApp/MusicListController.swift
+++ MusicApp/MusicListController.swift
@@ -82,7 +82,7 @@ extension MusicListController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let key = Character(sectionTitles![section])
-        return items?[key]!.count ?? 0
+        return items[key]?.count ?? 0
     }
 
     
