5eb7789fc073a6c5b2b2bcb61f6e192392bbf32d
Yehor <y.sorokin1996@gmail.com>
2019-10-17 10:53:14 +0300

    Update MusicLisControllerAVDelegate.swift

--- MusicApp/MusicLisControllerAVDelegate.swift
+++ MusicApp/MusicLisControllerAVDelegate.swift
@@ -12,23 +12,23 @@ import UIKit
 extension MusicListController: AVAudioPlayerDelegate {
     
     func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-        var key = Character(sectionTitles[playingKeyIndex])
+        var key = Character((sectionTitles?[playingKeyIndex!])!)
         
         //if we faced last item in section then jump to the first one on next section
-        if playingRow == items[key]!.count - 1 {
-            self.playingKeyIndex += 1
-            self.playingRow = 0
-            key = Character(sectionTitles[self.playingKeyIndex])
+        if playingRow == (items?[key]!.count)! - 1 {
+            self.playingKeyIndex? += 1
+            self.playingRow? = 0
+            key = Character(sectionTitles![playingKeyIndex!])
             
         } else {
-            self.playingRow += 1
+            self.playingRow? += 1
         }
         
         //play music and update playing view
-        prepareMusic(for: playingRow, at: key)
-        self.player.play()
+        prepareMusic(for: playingRow!, at: key)
+        self.player?.play()
         
-        updatePlayingView(with: items[key]![playingRow])
+        updatePlayingView(with: (items?[key]?[playingRow!])!)
     }
     
 }
